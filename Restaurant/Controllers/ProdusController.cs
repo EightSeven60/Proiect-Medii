76a218f05642be1205be9d4d5b97b9f72bb529ef
SabauDavid <davidnvm3@gmail.com>
2022-01-16 18:45:00 +0200

    Am adaugat view-uri pentru delete,edit si create la Produs.

--- Restaurant/Controllers/ProdusController.cs
+++ Restaurant/Controllers/ProdusController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Restaurant.Models;
-
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Restaurant.Helpers;
+using System.Data.Entity;
 namespace Restaurant.Controllers
 {
     public class ProdusController : Controller
@@ -15,5 +18,79 @@ namespace Restaurant.Controllers
         {
             return View(dbCtx.Produse.ToList());
         }
-    }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ProdusModel msg)
+        {
+            if (ModelState.IsValid)
+            {
+                dbCtx.Produse.Add(msg);
+                dbCtx.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            Task.Run(() => TraceWriter.WriteLineToTraceAsync("Model state was not valid in \"Create\" post method in \"Produs Controller\"."));
+            return View(msg);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                Task.Run(() => TraceWriter.WriteLineToTraceAsync("Id had no value in \"Edit\" get method in \"Produs Controller\"."));
+                return HttpNotFound();
+            }
+            ProdusModel produs = dbCtx.Produse.Find(id);
+            if (null == produs)
+            {
+                Task.Run(() => TraceWriter.WriteLineToTraceAsync("\"meniu\" was null in \"Edit\" get method in \"Produs Controller\"."));
+                return HttpNotFound();
+            }
+            return View(produs);
+        }
+        [HttpPost]
+        public ActionResult Edit(ProdusModel produs)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                dbCtx.Entry(produs).State = System.Data.Entity.EntityState.Modified;
+                dbCtx.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            Task.Run(() => TraceWriter.WriteLineToTraceAsync("Model state was not valid in \"Edit\" post method in \"Produs Controller\"."));
+            return View(produs);
+        }
+
+   
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            ProdusModel produs = dbCtx.Produse.Find(id);
+            if (produs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produs);
+        }
+
+  
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProdusModel produs = dbCtx.Produse.Find(id);
+            dbCtx.Produse.Remove(produs);
+            dbCtx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    } 
 }
+
