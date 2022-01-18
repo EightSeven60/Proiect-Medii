42c5a557af486751032fdac65053abbd457158c0
SabauDavid <davidnvm3@gmail.com>
2022-01-18 18:12:41 +0200

    Am adaugat user controller si view-urile necesare

--- Restaurant/Controllers/UserController.cs
+++ Restaurant/Controllers/UserController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Restaurant.Models;
-
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Restaurant.Helpers;
+using System.Data.Entity;
 namespace Restaurant.Controllers
 {
     public class UserController : Controller
@@ -15,25 +18,73 @@ namespace Restaurant.Controllers
         {
             return View(dbCtxt.Users.ToList());
         }
-        public ActionResult Logare()
+        public ActionResult Create(UserModel msg)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                dbCtxt.Users.Add(msg);
+                dbCtxt.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            Task.Run(() => TraceWriter.WriteLineToTraceAsync("Model state was not valid in \"Create\" post method in \"User Controller\"."));
+            return View(msg);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                Task.Run(() => TraceWriter.WriteLineToTraceAsync("Id had no value in \"Edit\" get method in \"User Controller\"."));
+                return HttpNotFound();
+            }
+            UserModel user = dbCtxt.Users.Find(id);
+            if (null == user)
+            {
+                Task.Run(() => TraceWriter.WriteLineToTraceAsync("\"user\" was null in \"Edit\" get method in \"User Controller\"."));
+                return HttpNotFound();
+            }
+            return View(user);
         }
         [HttpPost]
-        public ActionResult LogareResult()
+        public ActionResult Edit(UserModel user)
         {
-            string username = Request["txtUtilizator"].ToString();
-            string password = Request["txtPassword"].ToString();
-            if (password == "1234")
+            if (ModelState.IsValid)
             {
-                string mesaj = "Succes";
-                return Content(mesaj);
+
+                dbCtxt.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                dbCtxt.SaveChanges();
+                return RedirectToAction("Index");
+
             }
-            else
+            Task.Run(() => TraceWriter.WriteLineToTraceAsync("Model state was not valid in \"Edit\" post method in \"User Controller\"."));
+            return View(user);
+        }
+
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
             {
-                string mesaj = "Eroare";
-                return Content(mesaj);
+                return HttpNotFound();
             }
+            UserModel user = dbCtxt.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserModel user = dbCtxt.Users.Find(id);
+            dbCtxt.Users.Remove(user);
+            dbCtxt.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
