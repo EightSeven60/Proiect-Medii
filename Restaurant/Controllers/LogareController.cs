f3910c35492c88ee7f6cab0cbc1d151ba2d3cfbc
Samartean Rares Petru <endr60@yahoo.ro>
2022-01-19 18:20:29 +0200

    Implementat hashing de parole, verificare de username duplicat, implementat delogare si display reactiv pentru userul activ

--- Restaurant/Controllers/LogareController.cs
+++ Restaurant/Controllers/LogareController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Restaurant.Models;
+using System.Threading.Tasks;
+using Restaurant.Helpers;
 
 namespace Restaurant.Controllers
 {
@@ -22,6 +25,41 @@ namespace Restaurant.Controllers
         {
             string username = Request["txtUtilizator"].ToString();
             string password = Request["txtPassword"].ToString();
+
+            using (UserDbContext udb = new UserDbContext())
+            {
+                var users = from utilizatori in udb.Users
+                            where utilizatori.Username == username
+                            select utilizatori;
+                if (!users.Any())
+                {
+                    Task.Run(() => TraceWriter.WriteLineToTraceAsync("User introdus la logare negasit."));
+                    return Content("User incorect!");
+                }
+                else
+                {
+                    UserModel user = new UserModel();
+                    foreach (UserModel u in users)
+                    {
+                        user = u;
+                    }
+                    string hashedPassword = new CryptoHashHelper().GetHash(password);
+                    hashedPassword = new CryptoHashHelper().GetHash(password);
+                    hashedPassword = new CryptoHashHelper().GetHash(password);
+                    if (user.Password != hashedPassword)
+                    {
+                        Task.Run(() => TraceWriter.WriteLineToTraceAsync("Utilizatorul a introdus o parola gresita."));
+                        return Content("Parola incorecta.");
+                    }
+                    else
+                    {
+                        UserController.iduser = user.Id;
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            /*
             if (password == "1234")
             {
                 string mesaj = "Succes";
@@ -32,7 +70,12 @@ namespace Restaurant.Controllers
             {
                 string mesaj = "Eroare";
                 return Content(mesaj);
-            }
+            }*/
+        }
+        public ActionResult Delogare()
+        {
+            UserController.iduser = 0;
+            return RedirectToAction("Index", "Home");
         }
     }
 }
